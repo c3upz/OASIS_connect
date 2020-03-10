558b855721de44f7a834f119f09c9bcc9013987e
c3upz <cgerbino@scu.edu>
2020-03-10 11:02:23 -0700

    comments + little more styling

--- main.py
+++ main.py
@@ -13,50 +13,34 @@ import algos
 
 app = Flask(__name__)
 
+#defining the TWO routes the application needs
 
+#'/' route leads to the homepage and runs when the application launches
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# Flask tests
+#'/result' route leads to the results page where we display the results from the search term
 @app.route('/result')
 def results():
     spreadsheetSelection = request.args.get("spreadsheetDropDown")
     #spreadsheetSelection = int(spreadsheetSelection)
     print(spreadsheetSelection)
     typeSearch = request.args.get("searchbox")
-    print(typeSearch) #this now prints out whatever was put in the searchbox
+    print(typeSearch)  # this now prints out whatever was put in the searchbox
     dateSearch = 0
     dateSearch = request.args.get("dateDropDown")
-    if dateSearch != '0':
+    #this allows for the date search field to be left blank
+    if dateSearch != '0': #if value is not '0' then make it an int bc the user has selected a date
         dateSearch = int(dateSearch)
-    else:
+    else: #otherwise the user selected '-' which cooresponds to value '0'.. here the user does not want to search by year
         dateSearch = ""
-    print(dateSearch)
+    #searchInput is an array of the search terms the user has inputed
     searchInput = [typeSearch,  dateSearch, '']
-    print (searchInput)
-    # myJSON = api.GET_REQUEST('Mentor')
-    myJSON = api.GET_REQUEST(spreadsheetSelection)
-    myOut = algos.search_function(myJSON, searchInput)
-    print(myOut) #prints the array of ppl that it is returning
-    return render_template('results.html', myJSON = myOut)
-
-
-# @app.route("/modulesTest")
-# def modulesTest():
-#   firstName,lastName = userInfo.get_specific_userInfo(1, 'Mentor')
-#   return render_template('modulesTest.html', firstName=firstName, lastName=lastName)
-
-# @app.route('/forms', methods=['POST'])
-# def my_form_data():
-#   text = request.form['text']
-#   processed_text = text.upper()
-#   return processed_text
-
-# @app.route("/html_req_test")
-# def getEmail():
-#   email = html_req_test()
-#   return render_template('html_req_test.html', email=email)
+    myJSON = api.GET_REQUEST(spreadsheetSelection) #loads the spreadsheet data for given spreadsheet
+    myOut = algos.search_function(myJSON, searchInput) #output after data was compared to search terms
+    #print(myOut)  # prints the array of ppl that it is returning
+    return render_template('results.html', myJSON=myOut)
 
 
 if __name__ == "__main__":
